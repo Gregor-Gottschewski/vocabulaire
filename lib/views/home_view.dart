369a0d47863ea3c46752506dd3152345ab051d37
Gregor <contact@gregors-computer-lab.com>
2026-05-13 18:20:56 +0200

    minor(home_view): moved 'Add Box'-button to upper navbar

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -6,84 +6,68 @@ import 'package:vocabulaire/views/box_tile.dart';
 import '../controllers/box_controller.dart';
 import '../models/vocabulary_box.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
 
   @override
+  State<HomeView> createState() => HomeViewWidget();
+}
+
+class HomeViewWidget extends State<HomeView> {
+  final BoxController _boxController = BoxController();
+
+  void _addNewBox() {
+    var box = VocabularyBox(
+      name: 'Neue Box ${DateTime.now().millisecondsSinceEpoch}',
+      description: 'Beschreibung der neuen Box',
+      vocabularies: [],
+    );
+
+    _boxController.addBox(box);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final BoxController boxController = BoxController();
-
     return CupertinoPageScaffold(
-      navigationBar: CupertinoNavigationBar(middle: Text('Boxen')),
+      navigationBar: CupertinoNavigationBar(
+        middle: Text('Boxen'),
+        trailing: CupertinoButton(
+          padding: EdgeInsets.zero,
+          onPressed: _addNewBox,
+          child: const Icon(CupertinoIcons.add),
+        ),
+      ),
       child: SafeArea(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 16.0,
-                vertical: 8.0,
-              ),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: CupertinoButton.filled(
-                      onPressed: () {
-                        var box = VocabularyBox(
-                          name:
-                              'Neue Box ${DateTime.now().millisecondsSinceEpoch}',
-                          description: 'Beschreibung der neuen Box',
-                          vocabularies: [],
-                        );
+        child: Expanded(
+          child: ValueListenableBuilder(
+            valueListenable: _boxController.listenable,
+            builder: (context, Box<VocabularyBox> box, _) {
+              final keys = box.keys.cast<dynamic>().toList();
 
-                        boxController.addBox(box);
-                      },
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        mainAxisSize: MainAxisSize.min,
-                        children: const [
-                          Icon(CupertinoIcons.add, size: 20.0),
-                          SizedBox(width: 8.0),
-                          Text('Neue Box'),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
+              if (keys.isEmpty) {
+                return const Center(child: Text('Keine Boxen vorhanden.'));
+              }
 
-            Expanded(
-              child: ValueListenableBuilder(
-                valueListenable: boxController.listenable,
-                builder: (context, Box<VocabularyBox> box, _) {
-                  final keys = box.keys.cast<dynamic>().toList();
-
-                  if (keys.isEmpty) {
-                    return const Center(child: Text('Keine Boxen vorhanden.'));
-                  }
-
-                  return ListView.builder(
-                    itemCount: keys.length,
-                    itemBuilder: (context, index) {
-                      final b = box.get(keys[index]) as VocabularyBox;
-                      return BoxTile(
-                        box: b,
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            CupertinoPageRoute(
-                              builder: (context) =>
-                                  BoxDetailPage(box: b, boxKey: keys[index]),
-                            ),
-                          );
-                        },
+              return ListView.builder(
+                itemCount: keys.length,
+                itemBuilder: (context, index) {
+                  final b = box.get(keys[index]) as VocabularyBox;
+                  return BoxTile(
+                    box: b,
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        CupertinoPageRoute(
+                          builder: (context) =>
+                              BoxDetailPage(box: b, boxKey: keys[index]),
+                        ),
                       );
                     },
                   );
                 },
-              ),
-            ),
-          ],
+              );
+            },
+          ),
         ),
       ),
     );
