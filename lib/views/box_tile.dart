612023a406ccb59536ec007c9d9e9eedddec4ed7
Gregor <contact@gregors-computer-lab.com>
2026-05-25 20:04:13 +0200

    minor: update README with Hive `build_runner` information and minor code fix

--- lib/views/box_tile.dart
+++ lib/views/box_tile.dart
@@ -21,7 +21,7 @@ class BoxTile extends StatelessWidget {
             borderRadius: BorderRadius.circular(12.0),
             boxShadow: [
               BoxShadow(
-                color: Colors.black.withOpacity(0.1),
+                color: Colors.black.withValues(alpha: 0.1),
                 blurRadius: 6.0,
                 offset: const Offset(0, 3),
               ),
